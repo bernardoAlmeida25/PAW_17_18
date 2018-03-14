d8bdda7597cb7ead80ce0f431f251a76665f638a
Bernardo Almeida <8150060@estg.ipp.pt>
2018-03-14 14:56:40 +0000

    Arrays

---
+++ F03/Arrays/newYearsResolutions.js
@@ -0,0 +1,4 @@
+let newYearsResolutions = ['Run a marathon', 'Learn a new language', 'Learn a new language'];
+newYearsResolutions.push('Comer', 'Vinho verde');
+newYearsResolutions.pop()
+console.log(newYearsResolutions);
